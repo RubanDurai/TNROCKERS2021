1d1ca7caac38c564ab445586923968bebd9ab548
TN Botz <77948100+rubandurai27@users.noreply.github.com>
2022-10-10 12:23:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
- <script>
+
 <!-- Display Current Date  -->
 date = new Date();
 year = date.getFullYear();
 month = date.getMonth() + 1;
 day = date.getDate();
 document.getElementById("current_date").innerHTML = month + "/" + day + "/" + year;
-</script>
-<script>
+
 <!-- Display Current Day -->
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 const d = new Date();
 let day = days[d.getDay()];
 document.getElementById("current_day").innerHTML = day;
-</script>
-<script>
+
 	var myquote = ["சாவு நெனைச்சா வரும் சாதனை ஜெயிச்சா தான் வரும்",
                    "நீ படிச்ச School-ல நான் Headmaster",
                    "நீ பொழப்புக்குத்தான் ரவுடி நான் பொறந்ததுல இருந்தே ரவுடி",
                    "நான் ஊதுனா அணையுற தீக்குச்சி இல்லை சூரியன்"]
 var randomItem = myquote[Math.floor(Math.random() * myquote.length)];
 document.getElementById("myquote").innerHTML = randomItem
-</script>
+
